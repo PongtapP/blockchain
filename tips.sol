4b18c4db5b2fad67f40b21296cbfe5d4af2ab89e
PongtapP <653070310210@npu.ac.th>
2024-08-20 16:42:18 +0700

    remove waitress & distribute tips

--- tips.sol
+++ tips.sol
@@ -24,6 +24,7 @@ contract tips {
     }
     //3.2 add waitress
     function addWaitress(address payable walletAddress,string memory name) public {
+        require(msg.sender == owner, "Only the onwer can call this function.");
         bool waitressExist = false;
         if(waitress.length >=1){
             for(uint i=0; i<waitress.length; i++){
@@ -38,7 +39,21 @@ contract tips {
     }
     
 
-    //4. remove user
+    //4. remove waitress
+    function removeWaitress(address payable walletAddress) public{
+        if(waitress.length>=1){
+            for(uint i=0; i<waitress.length; i++){
+                if(waitress[i].walletAddress==walletAddress){
+                    for(uint j=i; j<waitress.length-1; j++){
+                        waitress[j]=waitress[j+1];
+                    }
+                    waitress.pop();
+                    break;
+                }
+                
+            }
+        }
+    }
 
     //5. view waitress
     function viewWaitress() public view returns(Waitress[] memory) {
@@ -46,5 +61,17 @@ contract tips {
     }
 
     //6. distribute tips
-
+    function distrubiteTips() public {
+        require(address(this).balance > 0, "Insufficient balance in the contract.");
+        if(waitress.length>=1){
+            uint amount = address(this).balance / waitress.length;
+            for(uint i=0; i<waitress.length; i++){
+                transfer(waitress[i].walletAddress,amount);
+            }
+        }
+    }
+    //6.1 transfer money
+    function transfer(address payable walletAddress, uint amount) internal {
+        walletAddress.transfer(amount);
+    }
 }
